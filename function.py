0c20e40a2433192f39471c9e64033fea89a7b8d5
Abdul Qadeer <151870730+qadeerarain@users.noreply.github.com>
2024-01-10 18:04:21 +0500

    Add files via upload

---
+++ function.py
@@ -0,0 +1,35 @@
+def add(a,b ):
+    c=a+b
+    return c
+print("sum is",c)
+
+add(3,9) 
+
+
+
+def add():
+    print("enter a two numbers:")
+    a=int(input("enter a number:"))  
+    b=int(input("enter a number:"))
+    c=a+b
+    print("sum is",c)
+    add() 
+
+
+    def add(a,b):
+    c=a+b
+    print(f"sum is",c)
+
+    
+    add(3,4)
+    
+
+
+    def add():
+    print("enter a two numbers: "))
+    a=int(input("enter a number: "))
+    b=int(input("enter a number: "))
+    return c
+
+s=add()
+print(f"sum is",s)
